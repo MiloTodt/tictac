075c9e97bc11902c6ef05f1e7d827dac4bc4e260
milotodt <milo@milotodt.com>
2018-11-16 21:50:46 -0800

    cleanup

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -17,35 +17,35 @@ class Button extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="card container mt-3">
-          <div className="card-body">
-            <div className="row">
-            <div className="col-md-8 offset-md-2">
-              <center>
-                <button className="btn btn-primary" onClick={this.callApi}>
-                  Load players
-                </button>
-              </center>
-              <br/>
-              <center>
-              <table className="blueTable">
-                <tr>
-                  <th>Name</th>
-                  <th>Wins</th>
-                  <th>Losses</th>
-                  <th>Draws</th>
-                </tr>
-                {this.state.results.map(obj => (
+        <div>
+          <div className="card container mt-3">
+            <div className="card-body">
+              <div className="row">
+              <div className="col-md-8 offset-md-2">
+                <center>
+                  <button className="btn btn-primary" onClick={this.callApi}>
+                    Load players
+                  </button>
+                </center>
+                <br/>
+                <center>
+                <table className="blueTable">
                   <tr>
-                    <td> {obj.name} </td>
-                    <td> {obj.wins} </td>
-                    <td> {obj.losses} </td>
-                    <td> {obj.draws} </td>
+                    <th>Name</th>
+                    <th>Wins</th>
+                    <th>Losses</th>
+                    <th>Draws</th>
                   </tr>
-                ))}
-              </table>
-              < /center>
+                  {this.state.results.map(obj => (
+                    <tr>
+                      <td> {obj.name} </td>
+                      <td> {obj.wins} </td>
+                      <td> {obj.losses} </td>
+                      <td> {obj.draws} </td>
+                    </tr>
+                  ))}
+                </table>
+                </center>
               </div>
             </div>
           </div>
