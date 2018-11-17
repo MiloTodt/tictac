e5997dcfc2562530365fbc8489baaebde55bc2b4
milotodt <milo@milotodt.com>
2018-11-16 20:27:30 -0800

    style cleanup

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,61 +1,54 @@
 import React, { Component } from "react";
 
 class Button extends Component {
-    state = {
-        results: []
-    };
+  state = {
+    results: []
+  };
 
-    callApi = async () => {
-        const api_call = await fetch("http://localhost:3001/api/v1/players");
+  callApi = async () => {
+    const api_call = await fetch("http://localhost:3001/api/v1/players");
 
-        const data = await api_call.json();
+    const data = await api_call.json();
 
-        this.setState({
-            results: data
-        });
-    };
+    this.setState({
+      results: data
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <div className="card container mt-3">
-                    <div className="card-body">
-                        <div className="row">
-                            <center>
-                                <button className="btn btn-primary" onClick={this.callApi}>
-                                    Test Call!
+  render() {
+    return (
+      <div>
+        <div className="card container mt-3">
+          <div className="card-body">
+            <div className="row">
+              <center>
+                <button className="btn btn-primary" onClick={this.callApi}>
+                  Load players
                 </button>
-                                <br />
-                            </center>
-                            < table class = "blueTable" >
-                            <tr>
-                                <th>Name</th>
-                                <th>Wins</th>
-                                <th>Losses</th>
-                                <th>Draws</th>
-                            </tr>
-                                {
-                                    this.state.results.map(obj => < tr > < td > {
-                                        obj.name
-                                    } < /td>
-                                    <td>{obj.wins}</td>
-                                    < td > {
-                                        obj.losses
-                                    } < /td>
-                                    < td > {
-                                        obj.draws
-                                    } < /td>
-                                    </tr > )
-                                    
-                                }
-                            < /table>
-
-                        </div>
-                    </div>
-                </div>
+                <br />
+              </center>
+              <table className="blueTable">
+                <tr>
+                  <th>Name</th>
+                  <th>Wins</th>
+                  <th>Losses</th>
+                  <th>Draws</th>
+                </tr>
+                {this.state.results.map(obj => (
+                  <tr>
+                    <td> {obj.name}</td>
+                    <td>{obj.wins}</td>
+                    <td> {obj.losses} </td>
+                    <td> {obj.draws} </td>
+                  </tr>
+                ))}
+              </table>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Button;
