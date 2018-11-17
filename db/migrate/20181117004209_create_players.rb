9bfa311c86ada20eed6de75bff3e5cd3792733a5
milotodt <milo@milotodt.com>
2018-11-16 17:35:35 -0800

    rails setup, model created

---
+++ db/migrate/20181117004209_create_players.rb
@@ -0,0 +1,12 @@
+class CreatePlayers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.integer :wins
+      t.integer :losses
+      t.integer :draws
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Player.create(name: "Jeff Bezos", wins:5, losses:2, draws:0)
+Player.create(name: "Bill Gates", wins:1, losses:8, draws:3)
+Player.create(name: "Justin Trudeau", wins:42, losses:12, draws:10)
