e8cd5d36210ba5da30ceaaf18d1718a78a414530
milotodt <milo@milotodt.com>
2018-11-19 15:08:59 -0800

    Player spec setup

--- spec/models/players_spec.rb
+++ spec/models/players_spec.rb
@@ -1,9 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 # Test suite for the Item model
 RSpec.describe Player, type: :model do
+  before { @player = FactoryBot.build(:player) }
 
-  # Validation test
-  # ensure column name is present before saving
-  it { should validate_presence_of(:name) }
+  subject { @player }
+  it { should respond_to(:name) }
+  it { should respond_to(:wins) }
+  it { should respond_to(:losses) }
+  it { should respond_to(:draws) }
+  it { should be_valid }
+
+  describe 'test no name' do
+    it 'has no name' do
+      FactoryBot.build(:player, name: nil).should_not be_valid
+    end
+  end
 end
