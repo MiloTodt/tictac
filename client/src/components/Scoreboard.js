355ac6862d16de219d08754f7e844c0cbdf995cc
milotodt <milo@milotodt.com>
2018-11-19 19:24:27 -0800

    s

--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -29,104 +29,110 @@ class Scoreboard extends React.Component {
       .catch(error => console.log(error));
   }
   createPlayer(name, result) {
+    //Create a new player, sending result of their first match
     if (result === "win") {
       const wins = 1;
-      axios.post("/api/v1/players", { player: { name, wins } }).then(response => {
-        console.log(response);
-        const players = [...this.state.players, response.data];
-        this.setState(state => {
-          return { players: players };
+      axios
+        .post("/api/v1/players", { player: { name, wins } })
+        .then(response => {
+          console.log(response);
+          const players = [...this.state.players, response.data];
+          this.setState(state => {
+            return { players: players };
+          });
         });
-      });
     } else if (result === "loss") {
       const losses = 1;
-      axios.post("/api/v1/players", { player: { name, losses } }).then(response => {
-        console.log(response);
-        const players = [...this.state.players, response.data];
-        this.setState(state => {
-          return { players: players };
+      axios
+        .post("/api/v1/players", { player: { name, losses } })
+        .then(response => {
+          console.log(response);
+          const players = [...this.state.players, response.data];
+          this.setState(state => {
+            return { players: players };
+          });
         });
-      });
     } else if (result === "draw") {
       const draws = 1;
-      axios.post("/api/v1/players", { player: { name, draws } }).then(response => {
-        console.log(response);
-        const players = [...this.state.players, response.data];
-        this.setState(state => {
-          return { players: players };
+      axios
+        .post("/api/v1/players", { player: { name, draws } })
+        .then(response => {
+          console.log(response);
+          const players = [...this.state.players, response.data];
+          this.setState(state => {
+            return { players: players };
+          });
         });
-      });
     }
   }
   updatePlayer(name, result) {
     const id = this.playerIndex(name);
-
     if (id === 0) {
       //New player, first create in database
       this.createPlayer(name, result);
-    } else{
-    const players = this.state.players.slice();
-    const player = this.state.players[id - 1];
-    let wins = player.wins;
-    let losses = player.losses;
-    let draws = player.draws;
-    if (result === "win") {
-      wins = wins + 1;
-      axios
-        .put("/api/v1/players/" + id, {
-          player: {
-            wins
-          }
-        })
-        .then(response => {
-          console.log(response);
-          players[id - 1] = { id, name, wins, losses, draws };
-          this.setState(() => ({
-            players,
-            editingPlayerId: null
-          }));
-        })
-        .catch(error => console.log(error));
-    } else if (result === "loss") {
-      losses = losses + 1;
-      axios
-        .put("/api/v1/players/" + id, {
-          player: {
-            losses
-          }
-        })
-        .then(response => {
-          console.log(response);
-          players[id - 1] = { id, name, wins, losses, draws };
-          this.setState(() => ({
-            players,
-            editingPlayerId: null
-          }));
-        })
-        .catch(error => console.log(error));
-    } else if (result === "draw") {
-      draws = draws + 1;
-      axios
-        .put("/api/v1/players/" + id, {
-          player: {
-            draws
-          }
-        })
-        .then(response => {
-          console.log(response);
-          players[id - 1] = { id, name, wins, losses, draws };
-          this.setState(() => ({
-            players,
-            editingPlayerId: null
-          }));
-        })
-        .catch(error => console.log(error));
+    } else {
+      const players = this.state.players.slice();
+      const player = this.state.players[id - 1];
+      let wins = player.wins;
+      let losses = player.losses;
+      let draws = player.draws;
+      if (result === "win") {
+        wins = wins + 1;
+        axios
+          .put("/api/v1/players/" + id, {
+            player: {
+              wins
+            }
+          })
+          .then(response => {
+            console.log(response);
+            players[id - 1] = { id, name, wins, losses, draws };
+            this.setState(() => ({
+              players,
+              editingPlayerId: null
+            }));
+          })
+          .catch(error => console.log(error));
+      } else if (result === "loss") {
+        losses = losses + 1;
+        axios
+          .put("/api/v1/players/" + id, {
+            player: {
+              losses
+            }
+          })
+          .then(response => {
+            console.log(response);
+            players[id - 1] = { id, name, wins, losses, draws };
+            this.setState(() => ({
+              players,
+              editingPlayerId: null
+            }));
+          })
+          .catch(error => console.log(error));
+      } else if (result === "draw") {
+        draws = draws + 1;
+        axios
+          .put("/api/v1/players/" + id, {
+            player: {
+              draws
+            }
+          })
+          .then(response => {
+            console.log(response);
+            players[id - 1] = { id, name, wins, losses, draws };
+            this.setState(() => ({
+              players,
+              editingPlayerId: null
+            }));
+          })
+          .catch(error => console.log(error));
+      }
     }
   }
-}
 
   bezos = () => {
-    this.updatePlayer("test7", "win");
+    this.updatePlayer("test8", "win");
   };
 
   renderBezos() {
